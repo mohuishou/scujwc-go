cf6ef62ed50aab41d6994e28705c61f096d30341
mohuishou <1@lailin.xyz>
2017-02-09 00:03:28 +0800

    update 获取所有成绩

--- example/main.go
+++ example/main.go
@@ -1,26 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"encoding/json"
-
-	"../"
-)
+import "../"
 
 func main() {
 	s := scujwc.Jwc{}
 	s.Init(2014141453066, "lailin123")
 	_ = s.Login()
-	grade, e := s.GPA()
+	_, e := s.GPAAll()
 	if e != nil {
 		return
 	}
-	j, err := json.Marshal(grade)
-	if err != nil {
-		fmt.Println(err)
-	}
-	os.Stdout.Write(j)
+	// j, err := json.Marshal(grade)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// os.Stdout.Write(j)
 
 }
