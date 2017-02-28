d2d685eee991242960ec43e33487c9b4485fda16
mohuishou <1@lailin.xyz>
2017-03-01 02:14:43 +0800

    update

--- calendar_test.go
+++ calendar_test.go
@@ -3,20 +3,5 @@ package scujwc
 import "testing"
 
 func Test_1(t *testing.T) {
-	// var j Jwc
-	// err := j.Init(2014141453066, "lailin123")
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-	// ical, err := j.Calendar(2)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-	// f, err := os.Create("ical.ics")
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
 
-	// defer f.Close()
-	// f.Write(ical.Bytes())
 }
--- gpa_test.go
+++ gpa_test.go
@@ -5,13 +5,5 @@ import (
 )
 
 func TestGPA(t *testing.T) {
-	j, err := NewJwc(2014141453066, "lailin123")
-	if err != nil {
-		t.Fatal(err)
-	}
-	grade, err := j.GPA()
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Log(grade)
+
 }
