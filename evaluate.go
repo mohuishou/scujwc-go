2a16b6cc245be78d13f083860e0860d8c9a98aa5
mohuishou <1@lailin.xyz>
2017-03-02 12:38:53 +0800

    update

---
+++ evaluate.go
@@ -0,0 +1,10 @@
+package scujwc
+
+//EvaluateList 评教信息列表
+type EvaluateList struct {
+	Teacher    string `json:"teacher"`
+	CourseName string `json:"CourseName"`
+	Status     string `json:"teacher"`
+	Teacher    string `json:"teacher"`
+	Teacher    string `json:"teacher"`
+}
